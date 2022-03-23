7f2157203dd9eaca20c0f4e30d51e8a43d12e3df
Fabionic <98551868+Fabionic@users.noreply.github.com>
2022-03-23 13:20:22 +0100

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <a href="form.php"></a>
+</head>
+<body>
+
+<p>Merci <?php echo $_POST['user_first'] .''. $_POST['user_name']?>  de nous avoir contacté à propos de “<?php echo $_POST['select'] ?> ”.
+
+Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user_email'] ?> ou par téléphone au <?php echo $_POST['user_phone'] ?> dans les plus brefs délais pour traiter votre demande : 
+
+<?php echo $_POST['user_message'] ?>
+</p>
+</body>
+</html
