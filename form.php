9020e8c8e1f2b7a7d50dca664096c75975f7091b
Fabionic <98551868+Fabionic@users.noreply.github.com>
2022-03-23 13:18:11 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,90 +1,48 @@
-<!doctype html>
-
-<html lang="fr">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
-
-
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <a href="thanks.php"></a>
 </head>
-
-<form  action="/ma-page-de-traitement"  method="post" id="contactForm">
-
-<?php
-
-  echo  $_POST['user_name'];
-
-?>
-
-
+<body>
+  <form action="thanks.php" method="post">
     <div>
-
-      <label  for="prénom">Prénom :</label>
-
-      <input  type="text"  id="prénom"  name="firstname">
-
+      <label for="nom">Nom :</label>
+      <input type="text" id="nom" name="user_name">
     </div>
-
     <div>
-
-      <label  for="nom">Nom :</label>
-
-      <input  type="text"  id="nom"  name="lastname">
-
+      <label for="prenom">Prénom :</label>
+      <input type="text" id="prenom" name="user_first">
     </div>
-
     <div>
-
-      <input type="select">
-      
-        <libellé>Quel est votre sport préféré ?</libellé>
-
-      <select size="1"> 
-        
-        <option valeur="fr">Pétanque</option>
-        <option valeur="nl">Bowling</option>
-        <option valeur="en">Natation</option>
-        <option valeur="other">Autre</option>
+      <label for="courriel">Courriel :</label>
+      <input type="email" id="courriel" name="user_email">
+    </div>
+    <div>
+      <label for="phone">Télephone :</label>
+      <input type="tel" id="phone" name="user_phone">
+    </div>
+    <div>
+      <label for="sujetselect" id="sujetselect"></label>
+      <select id="select" name="select">
+        <option value="information" selected>Information</option>
+        <option value="recruitment">Recrutement</option>
+        <option value="other">Autres</option>
       </select>
-      </input>
-    
-    </div> 
-
+    </div>
     <div>
-
-      <label  for="courriel">Courriel :</label>
-
-        <input  type="email"  id="courriel"  name="user_email">
-
+      <label for="message">Message :</label>
+      <textarea id="message" name="user_message"></textarea>
     </div>
-
-    <div>
-
-      <label  for="téléphone">Téléphone :</label>
-
-        <input  type="phone"  id="téléphone"  name="user_phone">
-
+    <div class="button">
+      <button type="submit">Envoyer votre message</button>
     </div>
-
-    <div>
-
-      <label  for="message">Message :</label>
-
-      <textarea  id="message"  name="user_message"></textarea>
-
-    </div>
-
-    <div  class="button">
-
-      <button  type="submit">Envoyer votre message</button>
-
-    </div>
-    
   </form>
-  
-  </html>
+</body>
+</html>
 
 
