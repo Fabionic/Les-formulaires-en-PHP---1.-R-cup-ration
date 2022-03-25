b0e722319c29dd20b3fd2596d82641caa06c2b75
Fabionic <98551868+Fabionic@users.noreply.github.com>
2022-03-25 16:59:32 +0100

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -17,3 +17,62 @@ Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user
 </p>
 </body>
 </html
+
+    
+    <?php
+
+// foreach($_POST as $key => $value) {
+//     $contact[$key] = trim($value);
+// }
+
+$errors = [];
+$countries = [
+    'france' => 'France',
+    'italy' => 'Italie',
+    'ukraine' => 'Ukraine',
+    'spain' => 'Espagne',
+];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $contact = array_map('trim', $_POST);
+
+    if (empty($contact['firstname'])) {
+        $errors[] = 'Le prénom est obligatoire';
+    }
+
+    $firstnameMaxLength = 70;
+    if (strlen($contact['firstname']) > $firstnameMaxLength) {
+        $errors[] = 'Le prénom doit faire moins de ' . $firstnameMaxLength . ' caractères';
+    }
+
+    if (empty($contact['email'])) {
+        $errors[] = 'L\'email est obligatoire';
+    }
+
+    $emailMaxLength = 255;
+    if (strlen($contact['email']) > $emailMaxLength) {
+        $errors[] = 'Le mail doit faire moins de ' . $emailMaxLength . ' caractères';
+    }
+
+    if (!filter_var($contact['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Mauvais format pour l\'email ' . htmlentities($contact['email']);
+    }
+
+    if (empty($contact['message'])) {
+        $errors[] = 'Le message est obligatoire';
+    }
+
+    if (!key_exists($contact['country'], $countries)) {
+        $errors[] = 'Le pays est invalide';
+    }
+
+    if (empty($errors)) {
+        // traitement de mon form
+        // insert en bdd
+        // envoie de mail...
+
+        header('Location: /index.php'); // redirection en GET (vide le POST)
+    }
+}
+?>
