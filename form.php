64417c234c8881c073e62905a78274326f4b5155
Fabionic <98551868+Fabionic@users.noreply.github.com>
2022-03-25 16:54:08 +0100

    Update form.php

--- form.php
+++ form.php
@@ -9,6 +9,34 @@
   <a href="thanks.php"></a>
 </head>
 <body>
+          <form action="" method="POST" novalidate>
+            <ul>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?= $error ?></li>
+                <?php endforeach; ?>
+            </ul>
+            <label for="firstname">Prénom</label>
+            <input type="text" id="firstname" name="firstname" placeholder="John Doe" required value="<?= $contact['firstname'] ?? '' ?>">
+
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" value="<?= $contact['email']  ?? '' ?>" placeholder="john@doe.com" required>
+
+            <label for="country">Pays</label>
+            <select name="country" id="country">
+                <?php foreach ($countries as $countryCode => $country) : ?>
+                    <option value="<?= $countryCode ?>" 
+                        <?= isset($contact['country']) && $contact['country'] === $countryCode ? 'selected' : '' ?>
+                    >
+                        <?= $country ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+
+            <label for="message">Message</label>
+            <textarea name="message" id="message" cols="30" rows="10" required><?= $contact['message'] ?? '' ?></textarea>
+            <div>max 3000 caractères</div>
+            <button>Envoyer</button>
+        </form>
   <form action="thanks.php" method="post">
     <div>
       <label for="nom">Nom :</label>
