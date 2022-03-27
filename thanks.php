50adc3a4545a5c53973e58c921777cf416602fb5
Fabionic <98551868+Fabionic@users.noreply.github.com>
2022-03-28 00:09:49 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -9,11 +9,11 @@
 </head>
 <body>
 
-<p>Merci <?php echo $_POST['user_first'] .''. $_POST['user_name']?>  de nous avoir contacté à propos de “<?php echo $_POST['select'] ?> ”.
+<p>Merci <?php echo $_POST['firstname'] .''. $_POST['lastname']?>  de nous avoir contacté à propos de “<?php echo $_POST['select'] ?> ”.
 
-Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user_email'] ?> ou par téléphone au <?php echo $_POST['user_phone'] ?> dans les plus brefs délais pour traiter votre demande : 
+Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['email'] ?> ou par téléphone au <?php echo $_POST['phone'] ?> dans les plus brefs délais pour traiter votre demande : 
 
-<?php echo $_POST['user_message'] ?>
+<?php echo $_POST['message'] ?>
 </p>
 </body>
 </html
@@ -30,7 +30,7 @@ $countries = [
     'france' => 'France',
     'italy' => 'Italie',
     'ukraine' => 'Ukraine',
-    'spain' => 'Espagne',
+    
 ];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
